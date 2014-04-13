fe3c33b2c61b682b85c5d9ef5997d0e1ba6d7eda
Sebastian Fahnenschreiber <sebastian.fahnenschreiber@gmail.com>
2014-04-14 00:13:58 +0200

    color magic

--- newpage/newpage.js
+++ newpage/newpage.js
@@ -3,7 +3,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     chrome.topSites.get(function(arr) {
         chrome.storage.local.get(null, function(data) {
-            
+
             $("#list").render(arr, {
                 favicon: {
                     src: function(params) {
@@ -21,24 +21,44 @@ document.addEventListener("DOMContentLoaded", function() {
                     }
                 }
             });
-            
-            var colorThief = new ColorThief();
-            $(".favicon").on("load", function(e) {         
-                
+
+            var ct = new ColorThief();
+            $(".favicon").on("load", function(e) {
+
                 var $this = $(this);
-                var color = colorThief.getColor($this[0]);
-                
-                
-                
-                var rgb = "rgb(" + color[0] + "," + color[1] + "," + color[2] + ")"
-                var border = "2px solid " + rgb;
-                
-                $this.parent().siblings(".title").css({"border-bottom": border});
-                
-               console.log( $this.parent().css("border"))
-                
+                var color = new RainbowColor(ct.getColor($this[0]), 'rgb');
+
+
+                var textColor = new RainbowColor('#FFFFFF', 'hex').blend(color, 0.20);
+
+
+                $this.parent()
+                        .siblings(".title")
+                        .css({
+                            "border-bottom": "2px solid " + color.get('hex')
+                        });
+
+                $this.parents(".elem").hover(function() {
+
+                    $(this).css({
+                        "box-shadow": "0px 0px 2px 2px " + textColor.get('hex')
+                    });
+
+                    $(".title", this).css({
+                        "text-shadow": "0px 0px 1px" + textColor.get('hex')
+                    });
+
+                }, function() {
+                    $(this).css({"box-shadow": "none"});
+                    $(".title", this).css({"text-shadow": "none"});
+                })
+
+
+                $this.parent()
+                        .parent()
+
             });
-            
+
 
             $(".elem").on("click", function(a) {
                 chrome.tabs.captureVisibleTab(function(dataUrl) {
